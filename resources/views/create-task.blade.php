6542188557d65f793a07ccc873d8424a1f051164
Lubos Noska <lubos.noska12@gmail.com>
2024-06-26 15:42:50 +0200

    Style create and edit form

--- resources/views/create-task.blade.php
+++ resources/views/create-task.blade.php
@@ -6,21 +6,21 @@
 @section('content')
     <form action="{{ route("tasks.store") }}" method="POST">
         @csrf
-        <div>
-            <label for="text">Title</label> <br />
-            <input type="text" name="title" id="title" value="{{ old('title') }}" /> 
+        <div class="mb-4">
+            <label for="text" class="uppercase text-slate-700 mb-2">Title</label> <br />
+            <input type="text" name="title" id="title" value="{{ old('title') }}" @class(['shadow-sm rounded-md appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none','border-red-500' => $errors->has('title')]) /> 
             @error('title')
-                <p>{{ $message }}</p>
+                <p class="text-red-500 text-sm font-medium">{{ $message }}</p>
             @enderror
         </div>
-        <div>
-            <label for="body">Desciption</label> <br />
-            <textarea name="body" id="body">{{ old('body') }}</textarea> 
+        <div class="mb-4">
+            <label for="body" class="uppercase text-slate-700 mb-2">Description</label> <br />
+            <textarea name="body" id="body" @class(['shadow-sm rounded-md appearance-none border w-full h-60 py-2 px-3 text-slate-700 leading-tight focus:outline-none','border-red-500' => $errors->has('body')])>{{ old('body') }}</textarea> 
             @error('body')
-                <p>{{ $message }}</p>
+                <p class="text-red-500 text-sm font-medium">{{ $message }}</p>
             @enderror
         </div>
-        <button type="submit">Add Task</button>
-        <a href="{{ route("tasks.index") }}">Cancel</a>
+        <button type="submit" class="rounded-md mr-2 px-2 py-1 text-center text-slate-700 shadow-sm ring-1 ring-slate-700/30 hover:bg-slate-50">Add Task</button>
+        <a href="{{ route("tasks.index") }}" class="underline decoration-pink-500">Cancel</a>
     </form>
 @endsection
--- resources/views/edit-task.blade.php
+++ resources/views/edit-task.blade.php
@@ -7,21 +7,21 @@
     <form action="{{ route("tasks.update", ["task" => $task->id]) }}" method="POST">
         @csrf
         @method("PUT")
-        <div>
-            <label for="text">Title</label> <br />
-            <input type="text" name="title" id="title" value="{{ old('title', $task->title) }}" /> 
+        <div class="mb-4">
+            <label for="text" class="uppercase text-slate-700 mb-2">Title</label> <br />
+            <input type="text" name="title" id="title" value="{{ old('title', $task->title) }}" class="shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none" /> 
             @error('title')
                 <p>{{ $message }}</p>
             @enderror
         </div>
-        <div>
-            <label for="body">Desciption</label> <br />
-            <textarea name="body" id="body">{{ old('body', $task->body) }}</textarea> 
+        <div class="mb-4">
+            <label for="body" class="uppercase text-slate-700 mb-2">Description</label> <br />
+            <textarea name="body" id="body" class="shadow-sm appearance-none border w-full h-60 py-2 px-3 text-slate-700 leading-tight focus:outline-none">{{ old('body', $task->body) }}</textarea> 
             @error('body')
                 <p>{{ $message }}</p>
             @enderror
         </div>
-        <button type="submit">Edit Task</button>
-        <a href="{{ route("tasks.index") }}">Cancel</a>
+        <button type="submit" class="rounded-md mr-2 px-2 py-1 text-center text-slate-700 shadow-sm ring-1 ring-slate-700/30 hover:bg-slate-50">Edit Task</button>
+        <a href="{{ route("tasks.index") }}" class="underline decoration-pink-500">Cancel</a>
     </form>
 @endsection
