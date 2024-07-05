60f5a4e0b7d99201f552fff2353f5a356b3d88c3
Lubos Noska <lubos.noska12@gmail.com>
2024-07-05 16:33:00 +0200

    Update avatar html

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,7 +7,21 @@
 
     <div class="mb-16">
         <hr class="mt-2 mb-4">
-        <h2 class="font-medium text-md mb-8 italic text-gray-500">Profile information</h2>
+        <h2 class="font-medium text-md mb-8">Update Avatar</h2>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="file" name="avatar" required /> <br />
+            @error('avatar')
+                <p class="text-red-500 text-sm font-medium">{{ $message }}</p>
+            @enderror
+            <x-button class="mr-2 mt-6">Update avatar</x-button>
+        </form>
+    </div>
+
+    <div class="mb-16">
+        <hr class="mt-2 mb-4">
+        <h2 class="font-medium text-md mb-8">Profile information</h2>
 
         <form action="" method="POST">
             @csrf
@@ -15,12 +29,12 @@
             <x-forms.input name="email" label="Email" type="email">{{ $user->email }}</x-forms.input>
             <x-button class="mr-2 mt-2">Update</x-button>
         </form>
-
     </div>
+
     <div class="">
-        
         <hr class="mt-2 mb-4">
-        <h2 class="font-medium text-md mb-8 italic text-gray-500">Change Password</h2>
+        <h2 class="font-medium text-md mb-8">Change Password</h2>
+
         <form action="" method="POST">
             @csrf
             <x-forms.input name="current_password" label="Current Name" type="password" />
